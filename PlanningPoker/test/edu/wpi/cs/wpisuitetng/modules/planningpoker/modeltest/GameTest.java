7173d81825e6fb4a5c6d1719b231d0aa0726fe61
Bobby9002 <raedwards@wpi.edu>
2014-03-28 14:41:16 -0400

    raedwards: Add tests to entity manager and game test files for testing functionality of some methods

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/modeltest/GameTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/modeltest/GameTest.java
@@ -14,10 +14,8 @@ import org.junit.*;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Game;
 import static org.junit.Assert.*;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Game testing class for Planning Poker
@@ -26,12 +24,51 @@ import java.util.Date;
  * @version Mar 23, 2014
  */
 public class GameTest {
+	
+	Game game1;
+	Game game2;
+	List<String> userList1 = new ArrayList<String>();
+	List<String> userList2 = new ArrayList<String>();
+	
+	@Before
+	public void setUp(){
+		game1 = new Game();
+		game2 = new Game();
+		
+		userList1.add("Bob");
+		userList1.add("Bill");
+		userList1.add("Jack");
+		userList1.add("Jill");
+		
+		userList2.add("Will");
+		userList2.add("Mark");
+		
+		game1.setId(1);
+		game1.setName("Planning Poker 1");
+		game1.setGameCreator("Susan");
+		game1.setParticipants(userList1);
+		
+		game2.setId(2);
+		game2.setName("Planning Poker 2");
+		game2.setGameCreator("Jane");
+		game2.setParticipants(userList2);
+	}
+	
 	@Test
-	public void generateNameTest()
+	public void identifyGameBasedOnDifferentObjectsTest()
 	{
-		DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-		Date now = Calendar.getInstance().getTime();
-		Game g = new Game();
-		assertEquals(g.getName(), df.format(now));
+		assertTrue(game1.identify(1));
+		assertTrue(game1.identify("1"));
+		assertTrue(game2.identify(2));
+		assertTrue(game2.identify("2"));
+		assertFalse(game2.identify("32"));
+	}
+	
+	@Test
+	public void checkIfParticipantTest()
+	{
+		assertTrue(game1.isParticipant("Bob"));
+		assertTrue(game2.isParticipant("Will"));
+		assertFalse(game1.isParticipant("Daniel"));
 	}
 }
--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/modeltest/PlanningPokerEntityManagerTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/modeltest/PlanningPokerEntityManagerTest.java
@@ -9,7 +9,9 @@
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.modeltest;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -34,8 +36,10 @@ import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.PlanningPokerEntityMa
 public class PlanningPokerEntityManagerTest {
 	
 	MockData db;
-	Game game;
+	Game game1;
 	Game game2;
+	List<String> userList1 = new ArrayList<String>();
+	List<String> userList2 = new ArrayList<String>();
 	PlanningPokerEntityManager manager;
 	User dummyUser;
 	Session s1;
@@ -46,8 +50,27 @@ public class PlanningPokerEntityManagerTest {
 	
 	@Before
 	public void setUp(){
-		game = new Game();
+		game1 = new Game();
 		game2 = new Game();
+		
+		userList1.add("Bob");
+		userList1.add("Bill");
+		userList1.add("Jack");
+		userList1.add("Jill");
+		
+		userList2.add("Will");
+		userList2.add("Mark");
+		
+		game1.setId(1);
+		game1.setName("Planning Poker 1");
+		game1.setGameCreator("Susan");
+		game1.setParticipants(userList1);
+		
+		game2.setId(2);
+		game2.setName("Planning Poker 2");
+		game2.setGameCreator("Jane");
+		game2.setParticipants(userList2);
+		
 		User dummyUser = new User("Bob", "bob", "abc123", 1);
 		dummyUser.setRole(Role.ADMIN);
 		testProject = new Project("test", "1");
@@ -55,23 +78,24 @@ public class PlanningPokerEntityManagerTest {
 		s1 = new Session(dummyUser, testProject, mockSsid);
 		
 		db = new MockData(new HashSet<Object>());
-		db.save(game, testProject);
+		db.save(game1, testProject);
 		db.save(dummyUser);
 		db.save(game2, otherProject);
 		manager = new PlanningPokerEntityManager(db);
 	}
 	
 	@Test
-	public void AddGameEntityToDatabaseTest() throws WPISuiteException{
-		Game newGame = manager.makeEntity(s1, game.toJSON());
+	public void AddGameEntityToDatabaseTest() 
+			throws WPISuiteException{
+		Game newGame = manager.makeEntity(s1, game1.toJSON());
 		assertEquals(1, newGame.getID());
-		assertEquals("Game", newGame.getName());
+		assertEquals("Planning Poker 1", newGame.getName());
 		assertSame(db.retrieve(Game.class,  "id", 1).get(0), newGame);
 	}
 	
 	@Test
 	public void GetGameEntityFromDatabase() throws NotFoundException, WPISuiteException{
 		Game[] retrived = manager.getEntity(s1, "1");
-		assertSame(game, retrived[0]);
+		assertSame(game1, retrived[0]);
 	}
 }
