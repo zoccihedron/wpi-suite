df2d255798fbe5f1cca5f123eede79e179ce309f
Kownt <cgmcandrews@wpi.edu>
2014-03-31 16:50:59 -0400

    Add a facade manager to encapsulate all connections with Requirement Manager, put test functionality on Create Req button. Needs refactoring before merge. cgmcandrews , rdlarkins

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/SelectRequirementsPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/SelectRequirementsPanel.java
@@ -2,6 +2,7 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.view;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -12,12 +13,18 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.facade.RequirementManagerFacade;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
 public class SelectRequirementsPanel extends JPanel {
 	private JTable existingRequirementsTable;
 	private JTable requirementsToAddTable;
 
 	public SelectRequirementsPanel() {
-
 
 		// Parent Container
 		JSplitPane requirementsSelectionPane = new JSplitPane();
@@ -29,7 +36,7 @@ public class SelectRequirementsPanel extends JPanel {
 
 		// Top Half of split pane
 
-		String[] columnNames = {"ID", "Name", "Description" };
+		String[] columnNames = { "ID", "Name", "Description" };
 
 		Object[][] data = {};
 
@@ -37,25 +44,31 @@ public class SelectRequirementsPanel extends JPanel {
 		existingRequirementsPanel.add(existingRequirementsLabel);
 
 		JButton btnNewRequirement = new JButton("Create Requirement");
-		btnNewRequirement.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO
-				// Open that other pane
-			}
 
-		});
 		existingRequirementsPanel.add(btnNewRequirement);
 
 		existingRequirementsTable = new JTable(new DefaultTableModel(data,
 				columnNames));
 
 		// Filling with some initial data for testing
-		DefaultTableModel model = (DefaultTableModel) existingRequirementsTable
+		final DefaultTableModel model = (DefaultTableModel) existingRequirementsTable
 				.getModel();
-		model.addRow(new Object[] {"0", "Riley Req", "Very Tuff" });
-		model.addRow(new Object[] {"1", "Chris Req", "Tuff" });
-		model.addRow(new Object[] {"2", "Carson Req", "Not So Tuff" });
+		model.addRow(new Object[] { "0", "Riley Req", "Very Tuff" });
+		model.addRow(new Object[] { "1", "Chris Req", "Tuff" });
+		model.addRow(new Object[] { "2", "Carson Req", "Not So Tuff" });
+
+		btnNewRequirement.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				ArrayList<Requirement> existingRequirements = new ArrayList<Requirement>(
+						RequirementManagerFacade.getInstance().getRequirments());
+				for (Requirement req : existingRequirements) {
+					model.addRow(new Object[] { Integer.toString(req.getId()),
+							req.getName(), req.getDescription() });
+				}
+			}
+
+		});
 
 		JScrollPane existingRequirementsTablePanel = new JScrollPane(
 				existingRequirementsTable);
@@ -92,7 +105,7 @@ public class SelectRequirementsPanel extends JPanel {
 		});
 		pendingRequirementsPanel.add(btnRemoveSelectedReq);
 
-		String[] addColumnNames = {"ID", "Name", "Description" };
+		String[] addColumnNames = { "ID", "Name", "Description" };
 		Object[][] addData = {};
 
 		requirementsToAddTable = new JTable(new DefaultTableModel(addData,
@@ -130,7 +143,8 @@ public class SelectRequirementsPanel extends JPanel {
 			// Add the values to that other table
 			DefaultTableModel modelToAddTo = (DefaultTableModel) dest
 					.getModel();
-			modelToAddTo.addRow(new Object[] { values[0], values[1], values[2] });
+			modelToAddTo
+					.addRow(new Object[] { values[0], values[1], values[2] });
 		}
 
 	}
