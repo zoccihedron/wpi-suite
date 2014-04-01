f948e66fccae922a51b7c7edcc941e3dcd37acd2
Ian Shusdock <ishusdock@gmail.com>
2014-04-01 13:16:25 -0400

    Changes to overviewPanel -Removed the refresh button as the table auto-refreshes -The table now fills the tab completely regardless of window size -The table no longer displays ID (per Marty's preference) Committed by irshusdock

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/OverviewPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/OverviewPanel.java
@@ -6,10 +6,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 
 import javax.swing.border.BevelBorder;
 
@@ -32,8 +31,7 @@ import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
  */
 public class OverviewPanel extends JPanel {
 	private OverviewTable table;
-	private JButton refreshBtn; // TODO: remove button 
-	private final String[] colNames = {"ID", "Name", "Status", "Deadline", "Number of Requirements", "Owner"};
+	private final String[] colNames = {"Name", "Status", "Deadline", "Number of Requirements", "Owner"};
 	private JScrollPane scrollPane;
 	private PlanningPokerModel gamesModel;
 	private MainView mainView;
@@ -47,43 +45,33 @@ public class OverviewPanel extends JPanel {
 		this.gamesModel = gamesModel;
 
 		String[][] data = {};
-		refreshBtn = new JButton("Refresh");
-
-
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1;
-		c.weighty = 0;
+		setLayout(new BorderLayout());
 
 		table = new OverviewTable(data, colNames);
 		scrollPane = new JScrollPane(table);
 
-		// ID
-		table.getColumnModel().getColumn(0).setMaxWidth(40);
 
 		// Game Name
-		table.getColumnModel().getColumn(1).setMinWidth(240);
+		table.getColumnModel().getColumn(0).setMinWidth(240);
 		// Status
-		table.getColumnModel().getColumn(2).setMinWidth(85);
-		table.getColumnModel().getColumn(2).setMaxWidth(85);
+		table.getColumnModel().getColumn(1).setMinWidth(85);
+		table.getColumnModel().getColumn(1).setMaxWidth(85);
 
 		// Deadline
-		table.getColumnModel().getColumn(3).setMinWidth(200);
-		table.getColumnModel().getColumn(3).setMaxWidth(200);
+		table.getColumnModel().getColumn(2).setMinWidth(200);
+		table.getColumnModel().getColumn(2).setMaxWidth(200);
 
 		// Num of Requirements
-		table.getColumnModel().getColumn(4).setMinWidth(40);
-		table.getColumnModel().getColumn(4).setMaxWidth(120);
+		table.getColumnModel().getColumn(3).setMinWidth(40);
+		table.getColumnModel().getColumn(3).setMaxWidth(120);
 
 		// Game Creator
-		table.getColumnModel().getColumn(5).setMaxWidth(85);
-		table.getColumnModel().getColumn(5).setMaxWidth(200);
+		table.getColumnModel().getColumn(4).setMaxWidth(85);
+		table.getColumnModel().getColumn(4).setMaxWidth(200);
 
 		scrollPane.setPreferredSize(new Dimension(1000,500));
-		add(scrollPane, c);
-		add(refreshBtn);
-
-		refreshBtn.addActionListener(new GetGamesController(gamesModel, this, mainView));
+		add(scrollPane, BorderLayout.CENTER);
+		
 	}
 
 	/**
