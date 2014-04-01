55fafc2b16329239d9d686c47716e825a35842d8
Ryan Baker <ryan.da.baker@gmail.com>
2014-04-01 19:47:14 -0400

    Removed Git merge conflict lines

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/MainView.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/MainView.java
@@ -6,13 +6,8 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
-<<<<<<< HEAD
- * Contributors:
- *    Team Codon Bleu
-=======
  * Creator:
- *    Code On Bleu
->>>>>>> feature-newGameFunctionality
+ *    Team Code On Bleu
  ******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view;
