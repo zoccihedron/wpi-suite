0f132a19895c05763faa634857423409356ea028
Ryan Baker <ryan.da.baker@gmail.com>
2014-04-01 19:48:27 -0400

    Changed 'Contributors' from "Team Rolling Thunder"

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/OverviewTable.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/OverviewTable.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2013 WPI-Suite
+ * Copyright (c) 2014 WPI-Suite
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: Team Rolling Thunder
+ * Contributors: Team Code On Bleu
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.overview;
 
