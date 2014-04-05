a585c7ffd6d69c12dbb455361c5340e47636d0cd
unknown <brank@LBRANKW7.rpega.com>
2014-04-05 13:25:55 -0400

    added documentation to mainviewtabcontroller. klbrann rdlarkins

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/MainViewTabController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/MainViewTabController.java
@@ -7,27 +7,46 @@ import javax.swing.JPanel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.MainView;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.NewGamePanel;
 
-
+/**
+ * This is a controller created for the purpose of interfacing with the main view.
+ * There is only one instance of MainViewTabController.
+ * @author Codon Bleu
+ *
+ */
 public class MainViewTabController {
 	
+
 	private static MainViewTabController instance = null;
 	private MainView mainView = null;
 
+	
 	private MainViewTabController() {
 		
 	}
 	
+
+	/**
+	 * 
+	 * @return reference to MainViewTabController
+	 */
 	public static MainViewTabController getInstance() {
 		if (instance == null) {
 			instance = new MainViewTabController();
 		}
 		return instance;
 	}
-	
+
+	/**
+	 * 
+	 * @param mainView 
+	 */
 	public void setMainView(MainView mainView) {
 		this.mainView = mainView;
 	}
 	
+	/**
+	 * Adds a new instance of a newGamePanel to tab bar of mainView
+	 */
 	public void createGameTab() {
 		NewGamePanel newGamePanel = new NewGamePanel();
 		mainView.addTab("New Game", null, newGamePanel, null);
@@ -36,6 +55,10 @@ public class MainViewTabController {
 		mainView.setSelectedComponent(newGamePanel);
 	}
 	
+	/**
+	 * 
+	 * @param tabToClose
+	 */
 	public void closeTab(Component tabToClose) {
 		mainView.remove(tabToClose);
 	}
