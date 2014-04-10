86e3f47d3245c2e350bbde33de64a873f6838753
unknown <brank@LBRANKW7.rpega.com>
2014-04-10 18:21:05 -0400

    Added two new panels to overview view, one for game info and one for game reqs. Req table needs work. klbrann

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/GameSummaryPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/GameSummaryPanel.java
@@ -6,217 +6,85 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *    Team Codon Bleu
+ * Creator:
+ *    Code On Bleu
  ******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.overview;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
-import edu.wpi.cs.wpisuitetng.janeway.config.ConfigManager;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.controller.MainViewTabController;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.facade.RequirementManagerFacade;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Game;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Game.GameStatus;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 
-/** GameSummaryPanel is a class which displays the summary
- * of a game that can be chosen to edit and/or play by a user
+/**
+ * This class is a JPanel. 
+ * It contains both a summary of fields from a game and 
+ * a table of requirements in a game.
  * 
- * @author Team Code On Bleu
+ * @author Code On Bleu
  * @version 1.0
  */
-@SuppressWarnings("serial")
-public class GameSummaryPanel extends JPanel{
-	
-	JLabel titleLabel;
-	JLabel deadlineLabel;
-	JLabel descriptionLabel;
-	JButton playGameBtn;
-	JButton editGameBtn;
-	JTextArea requirementsList;
-	Game game;
-	JScrollPane scrollPane;
-	JPanel emptyPanel1, emptyPanel2;
-	
-	
-	public GameSummaryPanel() 
-	{
+@SuppressWarnings({ "serial" })
+public class GameSummaryPanel extends JPanel {
+
+	private final GameSummaryInfoPanel infoPanel;
+	private final GameSummaryReqPanel reqPanel;
+
+	/**
+	 * This constructor is to be used when starting from a new game
+	 * 
+	 * @param parent
+	 *            the parent panel for this panel
+	 */
+	public GameSummaryPanel() {
+		
 		this.setLayout(new GridBagLayout());
-		
 		final GridBagConstraints constraints = new GridBagConstraints();
+
+		infoPanel = new GameSummaryInfoPanel();
+		reqPanel = new GameSummaryReqPanel();
 		
-		titleLabel = new JLabel("Title");
 		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridwidth = 3;
-		constraints.weightx = 0;
-		constraints.weighty = 0;
+		constraints.anchor = GridBagConstraints.NORTH;
+		constraints.gridwidth = 1;
+		constraints.weightx = 1.0;
+		constraints.weighty = 0.0;
 		constraints.gridx = 0;
 		constraints.gridy = 0;
+		constraints.ipadx = 10;
 		constraints.ipady = 10;
-		//titleLabel.setBounds(6, 6, 295, 56);
-		add(titleLabel, constraints);
+		add(infoPanel, constraints);
 		
-		deadlineLabel = new JLabel("Deadline");
+		JPanel emptyPanel = new JPanel();
 		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridwidth = 3;
-		constraints.weightx = 0;
-		constraints.weighty = 0;
-		constraints.gridx = 3;
-		constraints.gridy = 0;
-		//deadlineLabel.setBounds(313, 6, 131, 56);
-		add(deadlineLabel, constraints);
-		
-		descriptionLabel = new JLabel("Description");
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridwidth = 6;
-		constraints.weightx = 0;
-		constraints.weighty = 0;
+		constraints.gridwidth = 1;
+		constraints.weightx = 1.0;
+		constraints.weighty = 0.0;
 		constraints.gridx = 0;
 		constraints.gridy = 1;
-		//descriptionLabel.setBounds(6, 74, 438, 56);
-		add(descriptionLabel, constraints);
+		constraints.ipadx = 10;
+		constraints.ipady = 10;
+		add(emptyPanel, constraints);
 		
-		playGameBtn = new JButton("Play Game");
 		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.anchor = GridBagConstraints.SOUTH;
 		constraints.gridwidth = 1;
-		constraints.weightx = 0;
-		constraints.weighty = 0;
-		constraints.gridx = 6;
-		constraints.gridy = 3;
-		constraints.ipady = 0;
-		//playGameBtn.setBounds(327, 265, 117, 29);
-		add(playGameBtn, constraints);
-		playGameBtn.setEnabled(false);
-		
-		playGameBtn.addActionListener(new ActionListener () {
-		
-			public void actionPerformed(ActionEvent e)
-			{
-				final MainViewTabController mvt = MainViewTabController.getInstance();
-				mvt.playGameTab(game);
-			}
-			
-		});
-		
-		editGameBtn = new JButton("Edit Game");
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridwidth = 1;
-		constraints.weightx = 0;
-		constraints.weighty = 0;
-		constraints.gridx = 5;
-		constraints.gridy = 3;
-		//editGameBtn.setBounds(184, 265, 117, 29);
-		add(editGameBtn, constraints);
-		editGameBtn.setEnabled(false);
-		
-		editGameBtn.addActionListener(new ActionListener () {
-			
-			public void actionPerformed(ActionEvent e)
-			{
-				final MainViewTabController mvt = MainViewTabController.getInstance();
-				mvt.createGameTab(game);
-			}
-			
-		});
-		
-		requirementsList = new JTextArea("Requirements");
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridwidth = 6;
-		constraints.weightx = 1;
-		constraints.weighty = 1;
+		constraints.weightx = 1.0;
+		constraints.weighty = 0.0;
 		constraints.gridx = 0;
 		constraints.gridy = 2;
-		//requirementsList.setBounds(6, 147, 438, 106);
-		scrollPane = new JScrollPane(requirementsList);
-		add(scrollPane, constraints);
-		requirementsList.setEditable(false);
-		
-		emptyPanel1 = new JPanel();
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridwidth = 1;
-		constraints.weightx = .5;
-		constraints.weighty = 0;
-		constraints.gridx = 2;
-		constraints.gridy = 3;
-		add(emptyPanel1, constraints);
-		
-		emptyPanel2 = new JPanel();
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridwidth = 1;
-		constraints.weightx = .5;
-		constraints.weighty = 0;
-		constraints.gridx = 3;
-		constraints.gridy = 3;
-		add(emptyPanel2, constraints);
+		constraints.ipadx = 10;
+		constraints.ipady = 10;
+		add(reqPanel, constraints);
 	}
 	
-	/** Function which takes a game as a parameter and
-	 * updates the summary of the game that is displayed
-	 * 
-	 * @param gme the game to update the panel information with
-	 */
-	public void updateSummary(Game gme)
-	{
-		game = gme;
-		titleLabel.setText("Game Name: " + game.getName());
-		deadlineLabel.setText("Deadline: " + game.getEnd().toString());
-		descriptionLabel.setText("Description: " + game.getDescription());
-		final String appendedReqs = GameSummaryPanel.getRequirementNames(game);
-		requirementsList.setText(appendedReqs);
-		
-		// if the game is a draft and the user is the creator
-		// allow it to be editable
-		if(game.getStatus().equals(GameStatus.DRAFT) && 
-				game.getGameCreator().equals(ConfigManager.getConfig().getUserName()))
-		{
-			editGameBtn.setEnabled(true);
-		}
-		else
-		{
-			editGameBtn.setEnabled(false);
-		}
-		
-		// if the game is started
-		if(game.getStatus().equals(GameStatus.IN_PROGRESS)) 
-		{
-			playGameBtn.setEnabled(true);
-		}
-		else
-		{
-			playGameBtn.setEnabled(false);
-		}
+	public void updateSummary(Game game){
+		infoPanel.updateInfoSummary(game);
+		reqPanel.updateReqSummary(game);
 	}
 
-	/** Function which returns a string with all requirement names
-	 * for the given game appended (with new lines) into ones
-	 * string
-	 * 
-	 * @param game holds the requirements that will be returned
-	 * @return String of appended requirement names for the game
-	 */
-	private static String getRequirementNames(Game game) {
-		String temp = "";
-
-		RequirementManagerFacade.getInstance();
-		final List<Requirement> reqs = RequirementManagerFacade.getInstance().getPreStoredRequirements();
-		for(Requirement r : reqs)
-		{
-			if(game.getRequirements().contains(r.getId())){
-				temp += r.getName() + "\n";
-			}
-		}
-		
-		return temp;
-	}
+	
 }
+
+	
