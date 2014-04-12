fd176d31c1919fefe350aeda7a0ee5fbdb1ce38e
Riley Larkins <rdlarkins@wpi.edu>
2014-04-12 14:36:15 -0400

    Fixed the JTree on the overview panel trying to cast Strings to Games when an empty catagory was selected. rdlarkins

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/ListGamePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/ListGamePanel.java
@@ -104,8 +104,10 @@ implements TreeSelectionListener {
 		}
 		if(node.isLeaf() && !node.isRoot()){
 			final Object nodeInfo = node.getUserObject();
-			final Game gme = (Game) nodeInfo;
-			OverviewPanelController.getInstance().updateGameSummary(gme);
+			if(nodeInfo instanceof Game) {
+				final Game gme = (Game) nodeInfo;
+				OverviewPanelController.getInstance().updateGameSummary(gme);
+			}
 		}
 		
 		if (node == null) return;
