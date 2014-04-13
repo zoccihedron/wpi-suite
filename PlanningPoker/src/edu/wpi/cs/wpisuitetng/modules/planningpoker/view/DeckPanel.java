5d107d1a4699bd5c1c914b07c0595d3117c035f9
Kownt <cgmcandrews@wpi.edu>
2014-04-13 11:10:09 -0400

    Add functionality to deck view, can now select multiple or single card, display error with no card, sums deck correctly. cgmcandrews

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/DeckPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/DeckPanel.java
@@ -14,6 +14,8 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.view;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -27,6 +29,7 @@ import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 
 import edu.wpi.cs.wpisuitetng.janeway.config.ConfigManager;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Deck;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Game;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.requirements.ScrollablePanel;
 
@@ -42,20 +45,46 @@ public class DeckPanel extends JScrollPane {
 	private ImageIcon img = null;
 	private final int CARD_WIDTH = 100;
 	private final int CARD_HEIGHT = 128;
-
+	private final ArrayList<JToggleButton> listOfButtons = new ArrayList<JToggleButton>();
+	private boolean isDeckView;
 	/**
 	 * Constructs the DeckPanel
 	 * 
 	 */
 	public DeckPanel() {
-		this.setViewportView(textVersion());
+		ArrayList<Integer> cards = new ArrayList<Integer>();
+		 cards.add(1);
+		 cards.add(3);
+		 cards.add(1);
+		 cards.add(3);
+		Deck test = new Deck("Test", false, cards);
+		this.setViewportView(deckVersion(test));
 	}
 
 	public String getEstimateField() {
 		return estimateField.getText();
 	}
 
+	/**
+	 * This function returns true if a deck is selected, or is in text version
+	 * @return true if a deck is selected
+	 */
+	public boolean hasDeckSelected(){
+		boolean result = true;
+		
+		if(isDeckView){
+			result = false;
+			for( JToggleButton button : listOfButtons){
+				if(button.isSelected()){
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+	
 	private JPanel textVersion() {
+		isDeckView = false;
 		final ScrollablePanel textPanel = new ScrollablePanel();
 		textPanel.setLayout(new GridBagLayout());
 		final GridBagConstraints constraints = new GridBagConstraints();
@@ -85,23 +114,18 @@ public class DeckPanel extends JScrollPane {
 	/**
 	 * This is a proof of concept Temp GUI there is no functionality programmed in it
 	 * However it may be useful as a basis.
+	 * @param test 
 	 * @return The panel with clickable cards
 	 */
-	private JPanel deckVersion() {
-
+	private JPanel deckVersion(final Deck deck) {
+		isDeckView = true;
 		final JPanel deckPanel = new JPanel();
 		deckPanel.setLayout(new GridBagLayout());
 		final GridBagConstraints constraints = new GridBagConstraints();
 
-		// This creates a deck choice
-		final ArrayList<Integer> cards = new ArrayList<Integer>();
-		cards.add(0);
-		cards.add(1);
-		cards.add(1);
-		cards.add(2);
-		cards.add(3);
-		cards.add(5);
-		cards.add(8);
+		//Populate the deck with the cards it contains.
+		final ArrayList<Integer> cards = (ArrayList<Integer>) deck.getCards();
+
 		try {
 			img = new ImageIcon(ImageIO.read(getClass().getResource(
 					"cardtemplate.png")));
@@ -110,7 +134,7 @@ public class DeckPanel extends JScrollPane {
 		}
 
 		for (int i = 0; i < cards.size(); i++) {
-			JToggleButton cardToAdd = new JToggleButton(Integer.toString(cards
+			final JToggleButton cardToAdd = new JToggleButton(Integer.toString(cards
 					.get(i)), img);
 			cardToAdd.setHorizontalTextPosition(SwingConstants.CENTER);
 			cardToAdd.setVerticalTextPosition(SwingConstants.CENTER);
@@ -119,12 +143,42 @@ public class DeckPanel extends JScrollPane {
 			constraints.gridy = 2;
 			constraints.gridwidth = 1;
 			constraints.insets = new Insets(0, 5, 0, 5);
+			cardToAdd.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					if(!deck.canSelectMultipleCards()){
+						clearPrevious(cardToAdd);
+					}
+					calculateSum();
+				}
+			});
+			listOfButtons.add(cardToAdd);
 			deckPanel.add(cardToAdd, constraints);
 		}
 
 		return deckPanel;
 	}
 
+	private void clearPrevious(JToggleButton cardToAdd) {
+		for( JToggleButton button : listOfButtons){
+			if(!button.equals(cardToAdd)){
+				button.setSelected(false);
+			}
+		}
+		
+	}
+	
+	private void calculateSum() {
+		int result = 0;
+		
+		for( JToggleButton button : listOfButtons){
+			if(button.isSelected()){
+				result += Integer.valueOf(button.getText());
+			}
+		}
+		estimateField.setText(Integer.toString(result));
+	}
+	
 	/**
 	 * Displays the old estimate made by the user in the voting text field.
 	 * 
@@ -148,6 +202,16 @@ public class DeckPanel extends JScrollPane {
 			estimateField.setText("");
 		}
 
+	}
+
+	/**
+	 * This function is used to rest the deck selection to a blank state 
+	 */
+	public void clearCardsSelected() {
+		for( JToggleButton button : listOfButtons){
+			button.setSelected(false);
+		}
+		
 	}
 
 }
