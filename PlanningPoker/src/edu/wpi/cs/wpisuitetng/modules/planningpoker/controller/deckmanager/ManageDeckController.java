0f8bfbb8130a07e5dcff1b09e4c4c8f7a050b148
jnallard <jnallard@wpi.edu>
2014-04-24 18:14:07 -0400

    Changed the ManageDeckController to handle server requests for storing the deck list. jnallard

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/deckmanager/ManageDeckController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/deckmanager/ManageDeckController.java
@@ -2,27 +2,94 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller.deckmanager;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.Timer;
 
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Deck;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Game;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.deckmanager.CardViewPanel;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.deckmanager.DeckControlsPanel;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.newgame.CreateGameInfoPanel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.RequestObserver;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+import edu.wpi.cs.wpisuitetng.network.models.IRequest;
 
 public class ManageDeckController {
 
-	private DeckControlsPanel deckControl = null;
-	private CardViewPanel cardView = null;
+	static ManageDeckController instance = null;
+	private List<Deck> decks = new ArrayList<Deck>();
 
-	public ManageDeckController() {
-		//deckControl = deckControlsPane;
-		//cardView = cardViewPane;
+	private ManageDeckController() {
+		// Creates an ActionListener to be used by the timer 
+		// to update decks every few seconds
+		final ActionListener actionListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+					if(Network.getInstance().getDefaultNetworkConfiguration() != null){
+						updateDecks();
+					}
+				}
+				catch(RuntimeException exception){
+					System.err.println(exception.getMessage());
+				}
+			}
+		};
+
+		// Timer will update the requirements every 2 seconds
+		final Timer timer = new Timer(2000, actionListener);
+		timer.start();
+	}
+
+	protected void updateDecks() {
+		// Send a request to the server to retrieve all decks
+		final Request request = Network.getInstance().makeRequest("planningpoker/deck",
+																	HttpMethod.GET); // GET is read
+		request.addObserver(new RequestObserver() {
+			
+			@Override
+			public void responseSuccess(IRequest iReq) {
+				final Deck[] deckArray = Deck.fromJsonArray(iReq.getResponse().getBody());
+				setDecks(new ArrayList<Deck>(Arrays.asList(deckArray)));
+			}
+			
+			@Override
+			public void responseError(IRequest iReq) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void fail(IRequest iReq, Exception exception) {
+				// TODO Auto-generated method stub
+				
+			}
+		}); // add an observer to process the response
+		request.send(); // send the request
+	}
+
+	public ManageDeckController getInstance(){
+		if(instance == null){
+			return new ManageDeckController();
+		}
+		else{
+			return instance;
+		}
 	}
 
 	/**
-	 * Updates the Card View panel with the given game
+	 * @return the decks
 	 */
-	public void updateCardView(Deck deck){
-		cardView.updateView(deck);
+	public List<Deck> getDecks() {
+		return decks;
+	}
+
+	/**
+	 * @param decks the decks to set
+	 */
+	public void setDecks(List<Deck> decks) {
+		this.decks = decks;
 	}
 }
