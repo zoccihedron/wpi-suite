002571c44382b9b390599b230b4a701ff591eb15
Ian Shusdock <ishusdock@gmail.com>
2014-04-28 15:54:13 -0400

    Audit for AddDeckRequestObserver.java

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/deckmanager/AddDeckRequestObserver.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/deckmanager/AddDeckRequestObserver.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2014 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Creator:
+ *    Code On Bleu
+ ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller.deckmanager;
 
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Deck;
@@ -5,17 +14,26 @@ import edu.wpi.cs.wpisuitetng.network.RequestObserver;
 import edu.wpi.cs.wpisuitetng.network.models.IRequest;
 import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
 
+/**
+ * Request Observer for the add deck controller
+ * @author Code on Bleu
+ * @version 1.0
+ */
 public class AddDeckRequestObserver implements RequestObserver {
-	private AddDeckController addDeckController;
+	private final AddDeckController addDeckController;
 	
+	/**
+	 * Constructor for AddDeckRequestObserver
+	 * @param addDeckController the controller for the request observer
+	 */
 	public AddDeckRequestObserver(AddDeckController addDeckController){
 		this.addDeckController = addDeckController;
 	}
 	
 	@Override
 	public void responseSuccess(IRequest iReq) {
-		ResponseModel response = iReq.getResponse();
-		Deck deck = Deck.fromJson(response.getBody());
+		final ResponseModel response = iReq.getResponse();
+		final Deck deck = Deck.fromJson(response.getBody());
 		addDeckController.deckCreationSuccessful(deck);
 	}
 
