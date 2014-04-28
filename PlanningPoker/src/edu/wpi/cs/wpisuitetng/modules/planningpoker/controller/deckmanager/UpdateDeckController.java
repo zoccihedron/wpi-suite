d26fedb2685a2d5ecdcf620361245901c75239d8
Ian Shusdock <ishusdock@gmail.com>
2014-04-28 16:19:06 -0400

    Audit for UpdateDeckController.java

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/deckmanager/UpdateDeckController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/deckmanager/UpdateDeckController.java
@@ -1,8 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2014 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Creator:
+ *    Code On Bleu
+ ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller.deckmanager;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Deck;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.deckmanager.DeckControlsPanel;
@@ -13,13 +21,17 @@ import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 /**
  * Controls the functionality for updating a deck.
  * @author Code On Bleu
- *
+ * @version 1.0
  */
 public class UpdateDeckController implements ActionListener{
 	private Deck updatedDeck;
-	private Deck originalDeck;
-	private DeckControlsPanel deckControlsPanel;
+	private final Deck originalDeck;
+	private final DeckControlsPanel deckControlsPanel;
 	
+	/**
+	 * Constructor for UpdateDeckController
+	 * @param deckControlsPanel the deck controls panel
+	 */
 	public UpdateDeckController(DeckControlsPanel deckControlsPanel){
 		this.deckControlsPanel = deckControlsPanel;
 		originalDeck = deckControlsPanel.getDeck();
@@ -41,6 +53,10 @@ public class UpdateDeckController implements ActionListener{
 		
 	}
 	
+	/**
+	 * Update all views and actionlisteners and lists with the saved deck
+	 * @param deck the deck whose name was changed
+	 */
 	public void successfulNameChange(Deck deck){
 		deckControlsPanel.setActionListeners(deck);
 		deckControlsPanel.getCardView().updateView(deck);
